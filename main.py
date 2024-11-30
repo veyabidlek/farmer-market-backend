3550de74db84aac14adf1657552754a63db7582c
Bektas Keldibayev <bektas.keldibayev@nu.edu.kz>
2024-11-30 17:35:56 +0500

    add firebase storage

--- main.py
+++ main.py
@@ -2,6 +2,9 @@ from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
 from routers import auth, admin, farmer, buyer, common, payments, deliveries
 from database import Base, engine
+from firebase_admin import credentials, initialize_app, storage
+import firebase_admin
+from fastapi import UploadFile, HTTPException
 
 # Create FastAPI instance
 app = FastAPI(title="Farmer Market System")
@@ -20,6 +23,38 @@ app.add_middleware(
     allow_methods=["GET", "POST", "PUT", "DELETE"],  # Allowed HTTP methods
     allow_headers=["*"],  # Allow all headers
 )
+
+
+#
+cred = credentials.Certificate("./farmer-market-bfef4-firebase-adminsdk-bhaks-8005068aa6.json")
+if not firebase_admin._apps:
+    initialize_app(cred, {"storageBucket": "farmer-market-bfef4.firebasestorage.app"})
+
+@app.post("/upload/")
+async def upload_file(file: UploadFile):
+    bucket = storage.bucket()
+    blob = bucket.blob(file.filename)
+
+    # Upload the file to Firebase Storage
+    blob.upload_from_string(await file.read(), content_type=file.content_type)
+
+    # Make the file publicly accessible
+    blob.make_public()
+
+    return {"file_url": blob.public_url}
+
+@app.get("/download/{filename}")
+def download_file(filename: str):
+    bucket = storage.bucket()
+    blob = bucket.blob(filename)
+
+    if not blob.exists():
+        raise HTTPException(status_code=404, detail="File not found")
+
+    url = blob.generate_signed_url(expiration=3600)  # 1 hour expiration
+    return {"download_url": url}
+
+#
 
 # Include routers
 app.include_router(auth.router, prefix="/auth", tags=["Authentication"])
